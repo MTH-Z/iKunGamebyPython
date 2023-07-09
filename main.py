2ad5ce33afecf7bc0f04976a872f0040e1a2db56
MTH-Z <108459033+MTH-Z@users.noreply.github.com>
2023-07-09 11:01:26 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys, json, random
 import pygame
+import pygame.mixer as mixer
+import Data.functions as functions
 
 from Data.iKun import IKun
 
@@ -7,12 +9,13 @@ from Data.iKun import IKun
 class Main:
     def __init__(self):
         pygame.init()
+        mixer.init(frequency=22050, size=-16, channels=2, buffer=4096)
         self._load_settings()
         self.screen = pygame.display.set_mode(
             (self.settings["screen_width"], self.settings["screen_height"]))
         pygame.display.set_caption("iKunGame2023")
         self.iKuns = pygame.sprite.Group()
-        self.temporary_iKuns = pygame.sprite.Group()
+        self.temporary_iKuns = []
         
         self._first_iKuns()
 
@@ -48,55 +51,47 @@ class Main:
         if event.key == pygame.K_a:
             a = 0
             for i in self.iKuns:
-                if i.rect.x / 200 == a and i.rect.y == 400:
+                if i.rect.x / 200 == a:
                     i.kill()
-                    self._move_iKuns()
                     self._new_iKun()
+                    functions.classic_click()
                 else:
-                    pass
+                    functions.defeat()
         if event.key == pygame.K_s:
             a = 1
             for i in self.iKuns:
-                if i.rect.x / 200 == a and i.rect.y == 400:
+                if i.rect.x / 200 == a:
                     i.kill()
-                    self._move_iKuns()
                     self._new_iKun()
+                    functions.classic_click()
                 else:
-                    pass
+                    functions.defeat()
         if event.key == pygame.K_d:
             a = 2
             for i in self.iKuns:
-                if i.rect.x / 200 == a and i.rect.y == 400:
+                if i.rect.x / 200 == a:
                     i.kill()
-                    self._move_iKuns()
                     self._new_iKun()
+                    functions.classic_click()
                 else:
-                    pass
+                    functions.defeat()
         if event.key == pygame.K_f:
             a = 3
             for i in self.iKuns:
-                if i.rect.x / 200 == a and i.rect.y == 400:
+                if i.rect.x / 200 == a:
                     i.kill()
-                    self._move_iKuns()
                     self._new_iKun()
+                    functions.classic_click()
                 else:
-                    pass
-
-    def _move_iKuns(self):
-        for i in iter(self.iKuns):
-            i.rect.x = i.rect.x
-            i.rect.y += 200
-            self.temporary_iKuns.add(i)
-        self.iKuns = self.temporary_iKuns
+                    functions.defeat()
 
     def _first_iKuns(self):
-        for i in range(3):
-            iKun = IKun(self)
-            iKun.x = random.randint(0, 3) * 200
-            iKun.y = i * 200
-            iKun.rect.x = iKun.x
-            iKun.rect.y = iKun.y
-            self.iKuns.add(iKun)
+        iKun = IKun(self)
+        iKun.x = random.randint(0, 3) * 200
+        iKun.y = 0
+        iKun.rect.x = iKun.x
+        iKun.rect.y = iKun.y
+        self.iKuns.add(iKun)
 
     def _new_iKun(self):
         iKun = IKun(self)
@@ -108,4 +103,5 @@ class Main:
 
 if __name__ == '__main__':
     main = Main()
+    functions.play_bgm()
     main.run_game()
