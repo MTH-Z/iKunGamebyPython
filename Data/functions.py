db650bc43c4fe6473c9013d7064e808851fda115
MTH-Z <108459033+MTH-Z@users.noreply.github.com>
2023-07-29 18:01:00 +0800

    Add files via upload

--- Data/functions.py
+++ Data/functions.py
@@ -1,10 +1,10 @@
-import sys, time
+import json, sys, time
 from pygame.mixer import Sound
 
 
 def play_bgm():
     bgm = Sound('Data/audio/bgm.mp3')
-    bgm.set_volume(0.2)
+    bgm.set_volume(0.5)
     bgm.play(loops=-1, maxtime=0, fade_ms=0)
 
 def classic_click():
@@ -18,15 +18,36 @@ def defeat(score):
     time.sleep(3.5)
     sys.exit()
 
-def victory():
+def victory(game_time, difficulty):
     victory = Sound('Data/audio/victory.wav')
     victory.play(loops=0, maxtime=0, fade_ms=0)
-    print("\rVICTORY")
+    difficulty = difficulty.title()
+    game_time = round(game_time, 2)
+    print(f"\rVICTORY Difficulty:{difficulty} Time:{game_time}s")
+    with open('Data/scores.json', encoding = 'utf-8') as scores:
+        s = json.load(scores)
+    if s["best_score"]["classic"][difficulty] < game_time:
+        s["best_score"]["classic"][difficulty] = game_time
+        with open('Data/scores.json', mode='w', encoding = 'utf-8') as scores:
+            json.dump(s, scores)
+    best_score = s["best_score"]["classic"][difficulty]
+    print(f"BestScore:{best_score}s ({difficulty})")
     time.sleep(1.5)
     sys.exit()
 
-def score_board(score):
-    print('\r', score, end='', flush=True)
+def clear_score_list():
+    with open('Data/scores.json', encoding = 'utf-8') as scores:
+        s = json.load(scores)
+    s["best_score"]["classic"]["Easy"] = 0
+    s["best_score"]["classic"]["Normal"] = 0
+    s["best_score"]["classic"]["Hard"] = 0
+    s["best_score"]["classic"]["Insane"] = 0
+    with open('Data/scores.json', mode='w', encoding = 'utf-8') as scores:
+        json.dump(s, scores)
+    print(' The highest score has been reset.', end='')
+
+def score_board(score, total_score):
+    print('\r', f"{score}/{total_score}", end='', flush=True)
 
 def timer():
     pass
